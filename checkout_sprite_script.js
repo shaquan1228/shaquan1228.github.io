9708c75ded9955ef48f5ee3a4b0c42d5715bdcfe
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 18:21:33 -0400

    added window.onclick and removed eventlistener

--- checkout_sprite_script.js
+++ checkout_sprite_script.js
@@ -28,7 +28,7 @@
 				var button = document.createElement('button');
 				button.appendChild(player.dom);
 				document.body.appendChild(button);
-				button.location.href = 'https://shaquan1228.github.io/home.html';
+				button.href = 'https://shaquan1228.github.io/home.html';
 			//	button.onclick = window.open("https://shaquan1228.github.io/home.html");
 
                         } );
