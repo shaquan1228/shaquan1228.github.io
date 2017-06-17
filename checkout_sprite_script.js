8510b8e27143f2731942feb77eb7aedaae6f579c
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 13:21:11 -0400

    adding checkout sprite

---
+++ checkout_sprite_script.js
@@ -0,0 +1,26 @@
+                        var loader = new THREE.FileLoader();
+                        loader.load( 'js/app.json', function ( text ) {
+
+
+                                var player = new APP.Player();
+                                player.load( JSON.parse( text ) );
+                                player.setSize( window.innerWidth-60, window.innerHeight-100);
+                                player.play();
+
+                                document.body.appendChild( player.dom );
+
+                                window.addEventListener( 'resize', function () {
+                                        player.setSize( window.innerWidth-60, window.innerHeight-10 );
+                                } );
+
+                                if ( location.search === '?edit' ) {
+                                        var button = document.createElement( 'a' );
+                                        button.id = 'edit';
+                                        button.href = 'https://threejs.org/editor/#file=' + location.href.split( '/' ).slice( 0, - 1 ).join( '/' ) + '/app.json';
+                                        button.target = '_self';
+                                        button.textContent = 'EDIT';
+                                        document.body.appendChild( button );
+                                }
+
+                        } );
+
