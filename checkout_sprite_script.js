d8019374aad224b2f737c90677b183358c99b26d
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 15:58:33 -0400

    f

--- checkout_sprite_script.js
+++ checkout_sprite_script.js
@@ -4,13 +4,13 @@
 
                                 var player = new APP.Player();
                                 player.load( JSON.parse( text ) );
-                                player.setSize( window.innerWidth-200, window.innerHeight-300);
+                                player.setSize( window.innerWidth-200, window.innerHeight-100);
                                 player.play();
 
                                 document.body.appendChild( player.dom );
 
                                 window.addEventListener( 'resize', function () {
-                                        player.setSize( window.innerWidth-200, window.innerHeight-300);
+                                        player.setSize( window.innerWidth-200, window.innerHeight-100);
                                 } );
 
                                 if ( location.search === '?edit' ) {
