4662a989d863bc1253acf0af1d047e1769b17cbf
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 16:35:40 -0400

    trying to fix checkout sprite

--- checkout_sprite_script.js
+++ checkout_sprite_script.js
@@ -4,13 +4,13 @@
 
                                 var player = new APP.Player();
                                 player.load( JSON.parse( text ) );
-                                player.setSize( window.innerWidth-150, window.innerHeight-200);
+                                player.setSize( window.innerWidth-100, window.innerHeight-200);
                                 player.play();
 
                                 document.body.appendChild( player.dom );
 
                                 window.addEventListener( 'resize', function () {
-                                        player.setSize( window.innerWidth-150, window.innerHeight-200);
+                                        player.setSize( window.innerWidth-100, window.innerHeight-200);
                                 } );
 
                                 if ( location.search === '?edit' ) {
