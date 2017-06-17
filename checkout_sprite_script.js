de3b21d76c105aaab1e24bbda8b0a7bbe374d158
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 15:49:50 -0400

    changing sprite

--- checkout_sprite_script.js
+++ checkout_sprite_script.js
@@ -4,13 +4,13 @@
 
                                 var player = new APP.Player();
                                 player.load( JSON.parse( text ) );
-                                player.setSize( window.innerWidth-300, window.innerHeight-300);
+                                player.setSize( window.innerWidth+200, window.innerHeight+200);
                                 player.play();
 
                                 document.body.appendChild( player.dom );
 
                                 window.addEventListener( 'resize', function () {
-                                        player.setSize( window.innerWidth-300, window.innerHeight-300);
+                                        player.setSize( window.innerWidth+300, window.innerHeight+300);
                                 } );
 
                                 if ( location.search === '?edit' ) {
