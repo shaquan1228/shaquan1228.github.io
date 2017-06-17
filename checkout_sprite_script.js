4dcb284349ef365a99df0c8d362bce21ed15cea0
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 17:22:20 -0400

    removed .href from eventlistener

--- checkout_sprite_script.js
+++ checkout_sprite_script.js
@@ -13,9 +13,7 @@
                                         player.setSize( window.innerWidth-100, window.innerHeight-200);
                                 } );
 
-				window.addEventListener(onclick, function(){
-					window.open("https://shaquan1228.github.io/home.html");
-				});
+				window.addEventListener( 'onclick', window.open("https://shaquan1228.github.io/home.html"));
                                 if ( location.search === '?edit' ) {
                                         var button = document.createElement( 'a' );
                                         button.id = 'edit';
