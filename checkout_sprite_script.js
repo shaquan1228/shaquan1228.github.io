24ee218c59430090e53784036b2cf11cf695a329
Shaquan Nelson <sdn2115@columbia.edu>
2017-06-17 18:11:01 -0400

    added window.onclick and removed eventlistener

--- checkout_sprite_script.js
+++ checkout_sprite_script.js
@@ -7,15 +7,16 @@
                                 player.setSize( window.innerWidth-100, window.innerHeight-200);
                                 player.play();
 
-                                document.body.appendChild( player.dom );
-
+                              //  document.body.appendChild( player.dom );
+				
+				document.body.appendChild(
                                 window.addEventListener( 'resize', function () {
                                         player.setSize( window.innerWidth-100, window.innerHeight-200);
                                 } );
 
 				document.onclick = window.open("https://shaquan1228.github.io/home.html");
 				
-                        //        if ( location.search === '?edit' ) {
+                                if ( location.search === '?edit' ) {
                                         var button = document.createElement( 'a' );
                                         button.id = 'edit';
                                         button.href = 'https://shaquan1228.github.io/#file=' + location.href.split( '/' ).slice( 0, - 1 ).join( '/' ) + '/home.html';
@@ -23,7 +24,12 @@
                                         button.textContent = 'EDIT';
                                         document.body.appendChild( button );
 				
-                          //      }
+                                }
+				var button = document.createElement('button');
+				button.appendChild(player.dom);
+				document.body.appendChild(button);
+				 button.href = 'https://shaquan1228.github.io/#file=' + location.href.split( '/' ).slice( 0, - 1 ).join( '/' ) + '/home.html';
+
 
                         } );
 
