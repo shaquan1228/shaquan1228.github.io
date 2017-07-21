573efcf33078608f6cf4ccbc647326caec8497e0
Shaquan Nelson <sdn2115@columbia.edu>
2017-07-20 22:15:50 -0400

    fixing buttons

---
+++ java/Item.java
@@ -0,0 +1,27 @@
+
+package java;
+
+public class Item{
+
+    /*
+        name: product name
+        url: image url
+
+     */
+    private String name;
+    public Item(){
+
+
+
+
+    }
+
+    public static void main(String[] args){
+
+    System.out.println("HelloWorld")
+
+    }
+
+
+
+}
---
+++ test.jsp
@@ -0,0 +1,12 @@
+<%@ page import="java/Java.Item"%>
+
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>JSP Page</title>
+</head>
+<body>
+
+</body>
+</html>
